6a5fc96a54e688c2d877b23e42af395c368d851d
ering30 <115221464+ering30@users.noreply.github.com>
2025-07-28 18:41:47 +1000

    config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig as testConfig } from "vitest/config";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from 'path';
 
 // Vite configuration
